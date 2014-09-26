96118d52ae3786c957b7e61e171c4e05045b7133
budparr <budparr@sonnetmedia.net>
2014-09-26 17:05:35 -0400

    add top pull down menu
    also made some houscleaning changes.
    
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- assets/js/concatenate-scripts.js
+++ assets/js/concatenate-scripts.js
@@ -78,64 +78,7 @@ if (document.getElementById("filter-clear")) {
 
 
 
-// TUMBLR API
 
-  $(document).ready(function() {
-    $.ajax({
-        url: "http://api.tumblr.com/v2/blog/aba-icc.tumblr.com/posts?api_key=712yUR7HNAL8B8YXhrRwvH3sCnAgjV74BlYezAD8KWtbtXM9Ij&limit=10",
-        dataType: 'jsonp',
-        success: function(results){
-          console.log(results);
-          $.each(results.response.posts, function(i, item){
-              //var src = item.photos.url; // first picture, first size
-              var caption = item.caption;
-              var body = item.body;
-              var text = item.text;
-              var description = item.description;
-              var source_title = item.source_title;
-              var source_title = item.source_title;
-              //$("<img/>").attr("src", src).appendTo("#wrapper").wrap('<div class="postImage"></div>').after('<span class="postCaption">' + body + '</div>');
-              if(item.title && item.post_url) {
-                  $('<h6><a href="' + item.url +   '" target="_blank">' +  item.title + '</a> <small><i class="icon-link-ext" style="font-size:.8em;"></i></small> </h6>').appendTo('#content_tumblr');
-                    }
-                    else if(item.title) {
-                      $('<h5>' + item.title + '</h5>').appendTo('#content_tumblr');
-                };
-
-
-              if(item.description) {
-                  $('<small>' + description + '</small>').appendTo('#content_tumblr');
-                };
-
-                if(item.text) {
-                  $('<blockquote><em><small>' + text + '</small></em></blockquote>').appendTo('#content_tumblr');
-                };
-
-                  if(item.source) {
-                  $('<small>&mdash;' + text + '</small>').appendTo('#content_tumblr');
-                };
-
-                 if(item.source_title) {
-                  $('<br /><small><strong>' + source_title + '</strong></small>').appendTo('#content_tumblr');
-                };
-
-
-
-
-
-                if(item.urlTK) {
-                  $('<a href="' + item.url +   '" target="_blank">LINK</a>').appendTo('#content_tumblr');
-                };
-                    $('<hr />').appendTo('#content_tumblr')
-              });
-            }
-          });
-
-        });
-
-
-
-//-END TUMBLR API
 
 
 
@@ -146,7 +89,7 @@ if (document.getElementById("filter-clear")) {
 
 // FULL SCREEN IMAGES
 {% include scripts/jquery.backstretch.min.js %}
-{% comment %}DOES THIS WORK{% endcomment %}
+
 {% capture splash_image %}{{ page.splash_image }}{% endcapture %}
 $("aside.sidebar-right").backstretch("/assets/img/document.write=(splash_image);",{fade:'fast'});
 {% include scripts/jquery.backstretch.local.html splash_image="ff" %}
--- index.md
+++ index.md
@@ -5,9 +5,8 @@ title: The ABA-ICC PROJECT
 include: "grids/grid-items-home.html"
 sharing: false
 published: true
-sidebar_heading: Latest News
 category: home
 tweets: 3
 order: 12
 excerpt: "An independent initiative of the ABA to support the International Criminal Court & US-ICC relations through advocacy, education, and practical legal assistance"
----	
+---
