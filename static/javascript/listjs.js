140606087db23154b548db8ab8edd6c811290fc2
budparr <budparr@sonnetmedia.net>
2015-03-11 21:14:03 -0400

    make state parties list show all countries
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- static/javascript/listjs.js
+++ static/javascript/listjs.js
@@ -1,6 +1,7 @@
 var options = {
       valueNames: [ 'name', 'tagline', 'last_name_sort', 'genre'],
       listClass: 'list-filter',
+      page: 1000
   };
   
 var countryList = new List('container-filter', options);
