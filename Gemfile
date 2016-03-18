d7687f0ff23b36e880479b2ac6b9a3fea9ac4465
budparr <budparr@sonnetmedia.net>
2016-03-18 18:19:24 -0400

    wip moving things around
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- Gemfile
+++ Gemfile
@@ -1,5 +1,17 @@
 source 'https://rubygems.org'
 # gem 'github-pages'
 gem 'jekyll'
-# gem 'travis'
-# gem 's3_website'
+
+group :jekyll_plugins do
+  gem "html-proofer"
+  gem 'jekyll-seo-tag'
+  # gem "jekyll-lunr-js-search"
+  # gem 'jekyll-assets'
+  # gem 'autoprefixer-rails'
+  #gem 'jekyll-imgix'
+  gem 'jekyll-sitemap'
+  #gem 'jekyll-archives'
+end
+# gem 'jekyll-paginate'
+# gem 'jekyll-picture-tag'
+# gem 'jekyll_pages_api_search'
---
+++ _app/js/site.js
@@ -0,0 +1,29 @@
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+
+$('.js-accordion-trigger').bind('click', function(e){
+  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-expanded');
+  e.preventDefault();
+});
+
+
+$('#tipue_search_input').tipuesearch({
+  highlightEveryTerm: true,
+  descriptiveWords: 100
+});
+   $('a:not([href^="http://www.aba-icc.org/"]):not([href^="#"]):not([href^="/"])').attr("target","_blank");
+
+
+// if the menu is hidden, then show it = if display none, then display block. Or  = if display block, none.
+
+//   If it's not hidden then hide it
