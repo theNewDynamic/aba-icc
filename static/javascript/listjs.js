f7a7e590a689c47bd50f160e4fe95d1dfac80418
budparr <budparr@sonnetmedia.net>
2016-03-29 17:25:36 -0400

    Revert "Merge branch 'master' of https://github.com/ABA-Center-for-Human-Rights/aba-icc into gh-pages"
    This reverts commit c38a2f52756c4eac44eebf462260d477f4e6e96f, reversing
    changes made to de70f90ba535f98737ccb32107fc66117a1e86d1.

--- static/javascript/listjs.js
+++
@@ -1,7 +0,0 @@
-var options = {
-      valueNames: [ 'name', 'tagline', 'last_name_sort', 'genre'],
-      listClass: 'list-filter',
-      page: 1000
-  };
-  
-var countryList = new List('container-filter', options);
